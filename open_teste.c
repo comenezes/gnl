158b2d04c1d6d5fc94d49791162a7389a789073f
comenezes <32822994+comenezes@users.noreply.github.com>
2022-11-04 22:54:19 -0300

    Add files via upload

--- open_teste.c
+++ open_teste.c
@@ -3,43 +3,59 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+char *get_next_line(int fd)
+{
+	char		*linha;
+	int			buffer = 50;
+	char		*conteudo;
+	int			i = 0;
+
+	linha = malloc(sizeof(char)*buffer);
+	if ((read(fd, linha, 0) < 0) || (buffer <= 0))
+	{
+		write(1,"Erro",4);
+		return (NULL);
+	}
+	i = read(fd, linha, buffer);
+	if (buffer >= 0)
+		linha[buffer] = '\0';
+	if (buffer <= 0)
+		linha[0] = '\0';
+	i = 0;
+//	printf("i -> %d\n",i);	
+	while (linha[i] != '\n' && linha[i] != '\0')
+	{
+		printf("%c", linha[i]);
+//		conteudo[i] = linha[i];
+		i++;
+//		printf("i -> %d\n",i);	
+		
+	}
+	return (conteudo);
+}
+
 int main() 
 { 
-	char	*myfile;
-	int		buffer;
 	char	*nextline;
-	int		i;
+	int		fd = open("foo.txt", O_RDONLY); 
+
+	if (fd < 0) 
+    { 
+        perror("c1"); 
+        exit(1); 
+    } 
+    printf("Valor do FD = %d\n\n", fd);
+   	
+	nextline = get_next_line(fd);
 	
-	buffer = 500; 
-	int fd1 = open("foo.txt", O_RDONLY); 
-    if (fd1 < 0) 
+	printf("%s", nextline);
+	
+    if (close(fd) < 0) 
     { 
         perror("c1"); 
         exit(1); 
     } 
-    printf("opened the fd = %d\n\n", fd1); 
-	buffer = read(fd1,myfile,buffer);
-//		{
-			myfile[buffer] = '\0';
-//	   		printf("Content %s \n\n", myfile);
-//		}
-//	else 
-//		printf("Error!");
-	i = 0;
-    while (i <= buffer)
-		{
-			if (myfile[i] == '\n')
-			{ 
-				write(1," <NL>!", 6);
-			}
-			write(1, myfile + i, 1);
-			i++;
-		}
-	// Using close system Call 
-    if (close(fd1) < 0) 
-    { 
-        perror("c1"); 
-        exit(1); 
-    } 
-    printf("close the fd.\n"); 
+   printf("close the fd.\n");
+   return (0);
 }
+
