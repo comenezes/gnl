689779ddae349a8de4792f6135c0b8423d2d0cf8
comenezes <32822994+comenezes@users.noreply.github.com>
2022-11-07 01:19:09 -0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,99 +1,161 @@
-#include<stdio.h> 
+#include<stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <string.h>
+#define BUFFER_SIZE 5
 
-
-/* SE O NUMERO DE bytes lidos for 
- * menor que o buffer, pode ser
- * que chegou ao final do arquivo.
- *  ssize_t read(int fildes , void * buf , size_t nbyte ); */
-
-
-char	*read_content(fd, content)
+size_t	ft_strlen(const char *ch)
 {
-	int	i;
-	
-	read(fd, content, BUFFER_SIZE);
-//verificar erros de leitura resultante da operação acima 
-//
-	return (content);
+    size_t     i;
+
+    i = 0;
+    while (ch[i] != '\0')
+        i++;
+    return (i);
 }
 
-char	*strip_line(content)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	// recebe o conteudo do arquivo e devolve 
-	// uma linha do mesmo
-	
+    int		i;
+    int		j;
+    char	*chjoin;
+
+    i = 0;
+    j = 0;
+    chjoin = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+    if (chjoin == NULL)
+        return (NULL);
+    while (s1[i])
+    {
+        chjoin[i] = s1[i];
+        i++;
+    }
+    while (s2[j])
+    {
+        chjoin[i + j] = s2[j];
+        j++;
+    }
+    chjoin[i + j] = '\0';
+    return (chjoin);
 }
 
+int ch_find_nl(char *text)
+{
+    int i;
+    i = 0;
+    while (text[i])
+    {
+        if (text[i] == '\n')
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+char	*read_content(int fd, char *content)
+{
+    int	nbytes;
+    char *temp;
+    char *cache;
+
+    temp = malloc(sizeof(char)*1);
+    if (!temp)
+        return(NULL);
+    nbytes = 1;
+    while (nbytes != 0 && !ch_find_nl(temp))
+    {
+        nbytes = read(fd, temp, BUFFER_SIZE);
+        if (nbytes == -1)
+        {
+            free(temp);
+            free(content);
+            return (NULL);
+        }
+        temp[nbytes] = '\0';
+
+        cache = ft_strjoin(content, temp);
+        free (content);
+        content = cache;
+    }
+    free(temp);
+    return (content);
+}
+
+char	*strip_line(char *content)
+{
+    int     i;
+    char    *temp;
+
+    i = 0;
+    while (content[i] != '\n' && content[i] != '\0')
+        i++;
+    temp = malloc(sizeof(char) * i + 2);
+    temp[i+2] = '\0';
+    temp[i+1] = '\n';
+    while (i >= 0)
+        {
+            temp[i] = content[i];
+            i--;
+        }
+//    printf("\n\n%s\n\n",temp);
+    return(temp);
+}
+
+/*
 char	*get_rest(content)
 {
-//	recebe o conteudo do arquivo remove a linha que foi lida e 
+//	recebe o conteudo do arquivo remove a linha que foi lida e
 //	guarda o restante para proxima leitura
 }
 
-char *getline(fd)
-{
-	char	*linha;
-	char	*conteudo;
-	int		i;
-
-	linha = malloc(sizeof(char)*BUFFER_SIZE);
-	conteudo = malloc(sizeof(char)*1);
-		i = 0;
-}
-
-
-int main() 
-{ 
-	char	*nextline;
-	int		fd = open("foo.txt", O_RDONLY); 
-
-	if (fd < 0) 
-    { 
-        perror("c1"); 
-        exit(1); 
-    } 
-//    printf("Valor do FD = %d\n\n", fd);
-   	
-	nextline = get_next_line(fd);
-	
-	printf("%s", nextline);
-	
-    if (close(fd) < 0) 
-    { 
-        perror("c1"); 
-        exit(1); 
-    } 
-   printf("\nclose the fd.\n");
-   return (0);
-}
-
+*/
 
 char *get_next_line(int fd)
 {
-	char		*line;
-	char		*content;
-	static char	*reserve;
-	int			i;
+    char		    *line;
+    static char		*content;
+//     char	*reserve;
+//   int			i;
 
-	if ((read(fd, 0, 0) < 0) || (BUFFER_SIZE <= 0))
-		return (NULL);
-	content = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (!content)
-		return (NULL);
-//conteudo abaixo é util na pós leitura do arquivo
-//	if (BUFFER_SIZE >= 0)
-//		content[BUFFER_SIZE] = '\0';
-	if (!reserve)
-		reserve = strdup("\0"); // ???
-	content = read_content(fd, content);
-	line = strip_line(content);
-	reserve = get_rest(content);	
-	}
-	free(content);
-	return (line);
+    if ((read(fd, 0, 0) < 0) || (BUFFER_SIZE <= 0))
+        return (NULL);
+    if (!content)
+    {
+        content = malloc(sizeof(char) * BUFFER_SIZE + 1);
+        if (!content)
+            return (NULL);
+    }
+    content = read_content(fd, content);
+    line = strip_line(content);
+//    reserve = get_rest(content);
+
+//    free(content);
+    return (line);
+}
+
+int main()
+{
+    char	*nextline;
+    int		fd = open("/Users/claudio/CLionProjects/gnl/texto.txt", O_RDONLY);
+
+    if (fd < 0)
+    {
+        perror("c1");
+        exit(1);
+    }
+
+	nextline = get_next_line(fd);
+    printf("%s", nextline);
+
+
+    if (close(fd) < 0)
+    {
+        perror("c1"); //atencao
+        exit(1);
+    }
+    printf("\n\nclose the fd.\n");
+    return (0);
 }
 
 /*char    *get_next_line(int fd)
@@ -101,7 +163,6 @@ char *get_next_line(int fd)
     char			*line;
     static char    *save;
     char        *buffer;
-
     if (read(fd, 0, 0) < 0 || BUFFER_SIZE <= 0)
         return (NULL);
     buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
